016d5409a80f350a7e307d35985171aeee4682ff
coder <coder@192.168.0.102>
2020-04-12 22:04:30 +0530

    for each

--- Photon/src/java8/ForEach.java
+++ Photon/src/java8/ForEach.java
@@ -1,7 +1,10 @@
 package java8;
 
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.function.BiConsumer;
 import java.util.function.Consumer;
 
 public class ForEach {
@@ -13,6 +16,7 @@ public class ForEach {
 		
 		//if you just want to print all the elements
 		name.forEach(System.out::println);
+		System.out.println("---------------------------------------------");
 		
 		//if you need to do some operations on it
 		Consumer<String> makeUpperCase = new Consumer<String>() {
@@ -23,7 +27,33 @@ public class ForEach {
 			
 		};
 		name.forEach(makeUpperCase);
-
+		System.out.println("---------------------------------------------");
+		
+		//iterating hashmap using foreach
+		Map<String, Integer> nameID = new HashMap<String, Integer>();
+		
+		nameID.put("priyal", 1);
+		nameID.put("ninad", 2);
+		nameID.put("nidhi", 3);
+		nameID.put("hirak", 4);
+		
+		nameID.forEach((k,v)->System.out.println("Key :"+k+", Value :"+v));
+		System.out.println("---------------------------------------------");
+		
+		//iterating hashmap using foreach and BIConsumer interface.
+		BiConsumer<String, Integer> printWithConsumer = (k,v) -> {
+			
+			System.out.println("key "+ k);
+			System.out.println("value "+ v);
+		};
+		nameID.forEach(printWithConsumer);
+		System.out.println("---------------------------------------------");
+		
+		//using stream API; giving conditions using lambda expressions and then finally using the forEach
+		Consumer<String> consumer = System.out::println;
+		name.stream().filter(n -> n!=null).forEach(consumer);
+		
+		
 	}
 
-}
+};
