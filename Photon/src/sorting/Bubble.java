72c5363e1252e883034d36a3df4c1bec5bb1aa36
Priyal03 <priyalpatel94@gmail.com>
2020-06-01 23:44:27 +0530

    linear time case added

--- Photon/src/sorting/Bubble.java
+++ Photon/src/sorting/Bubble.java
@@ -5,9 +5,11 @@ public class Bubble {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		int arr[] = { 1, 4, 3, -2, -6, 5 };
+		int arr[] = {1,2,3,4,5,6} ;//{ 1, 4, 3, -2, -6, 5 };
 
-		int separationIndex = arr.length;
+		int separationIndex = arr.length, it=0;
+		
+		boolean flag=false;
 		
 		for (int i = separationIndex-1; i >=0 ; i--) {//for n times
 
@@ -15,13 +17,21 @@ public class Bubble {
 
 				if (arr[j] > arr[j + 1]) {
 					swap(arr, j, j + 1);
+					flag=true;
+					
 				}
+				it++;
+			}
+			if(flag==false) {
+				break;
 			}
 			//each time sorted partition is increasing.. bcoz of that sorting partition index is reducing.
 		}
 		for (int i : arr) {
-			System.out.println(i);
+			System.out.print(i+" ");
 		}
+		
+		System.out.println("\n"+it);
 	}
 
 	public static void swap(int[] arr, int i, int j) {
